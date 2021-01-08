dce624c24a3a30f382a8206748452501a321c250
Roberto Llopis <robertollopis@msn.com>
2021-01-08 11:20:32 +0100

    Some styles adaptation

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -11,13 +11,22 @@
             <div class="likes-comments"><i class="far fa-heart mr-10"></i><i class="far fa-comment scale-x-100"></i></div>
             <i class="far fa-bookmark"></i>
         </div>
-        @if($post['likes_count'] > 1)
-        <div class="likes-info">{{$post['likes_count']}} Likes</div>
-        @endif
+        <div class="w-full">
+            <span class="likes-info">
+                @if($post['likes_count'] > 0)
+                {{$post['likes_count']}} Likes
+                @endif
+            </span>
+            <span class="comments-info">
+                @if($post['comments_count'] > 0)
+                {{$post['comments_count']}} comments
+                @endif
+            </span>
+        </div>
         <div class="article-description"><span class="description-user-name">{{$post['user']['name']}}</span> ipsum dolor sit, amet consectetur adipisicing elit. Earum repudiandae voluptatum corporis iure at cum, voluptate distinctio, quaerat non porro a eos doloremque deserunt eaque! Rerum quidem ratione eum quasi.</div>
         <div class="article-comments-cnt mt-2">
-            @if($post['comments_count'] > 1)
-            <p class="text-gray-500 underline cursor-pointer">See the <span class="number-of-comments">{{$post['comments_count']}}</span> comments </p>
+            @if($post['comments_count'] > 2)
+            <p class="text-gray-500 underline cursor-pointer">See more comments...</p>
             @endif
             @foreach($post['comments'] as $comment)
             <x-comment :comment="$comment" />
